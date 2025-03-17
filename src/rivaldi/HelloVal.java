fc23a2d95b3012d5a559edba75e07a75c6d6e46b
Rivaldi Surya <29rivaldisuryasaputra@gmail.com>
2025-03-17 21:36:33 +0700

    membuat branch rivaldi dan menambahkan helloval

---
+++ src/rivaldi/HelloVal.java
@@ -0,0 +1,8 @@
+package src.rivaldi;
+
+public class HelloVal {
+    public static void main(String[] args) {
+        System.out.println("HelloValdii");
+        System.out.println("Valdi");
+    }
+}
