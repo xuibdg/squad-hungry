748aa090070d2b6110b0a6db08a9d2fb3e26ca6e
Jauharram <jauharram@gmail.com>
2025-03-18 12:31:41 +0700

    sout markicobs b2camp java backend class!

--- out/production/squad-hungry/README.md
+++
@@ -1,1 +0,0 @@
-# squad-hungry
---
+++ src/Jauhalal/JauhalalClass.java
@@ -0,0 +1,8 @@
+package Jauhalal;
+
+public class JauhalalClass {
+    public static void main(String[] args) {
+        System.out.println("Markicobss!!");
+        System.out.println("B2Camp Backend Java Class!!");
+    }
+}
