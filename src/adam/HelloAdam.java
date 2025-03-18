354a3b66da4ef34f8d2c03184f82c8fb6fed17c9
MuhammadAdam31 <muhamadadam113@gmail.com>
2025-03-18 11:32:40 +0700

    Late

--- src/adam/HelloAdam.java
+++ src/adam/HelloAdam.java
@@ -4,5 +4,6 @@ public class HelloAdam {
     public static void main(String[] args) {
         System.out.println("HelloAdam");
         System.out.println("MyWorld");
+        System.out.println("RealWorld");
     }
 }
